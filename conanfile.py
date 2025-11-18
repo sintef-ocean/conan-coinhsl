d38702720327ff2c2dcbfc66c737a5b592452e93
Joakim Haugen <joakim.haugen@sintef.no>
2025-11-18 16:46:12 +0100

    Improve recipe and attempt windows support

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,6 @@
 import os
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.build import cross_building
 from conan.tools.files import get, unzip, copy, rmdir, rm
 from conan.tools.gnu import (
     Autotools, AutotoolsDeps, AutotoolsToolchain,
@@ -9,10 +8,9 @@ from conan.tools.gnu import (
     )
 from conan.tools.meson import Meson, MesonToolchain
 from conan.tools.layout import basic_layout
-from conan.tools.microsoft.visual import is_msvc
+from conan.tools.microsoft import is_msvc, unix_path
 from conan.tools.scm import Version
-from conan.tools.env import VirtualRunEnv
-
+from conan.tools.system.package_manager import Apt
 
 required_conan_version = ">=2.18.0"
 
@@ -41,6 +39,10 @@ class CoinHslConan(ConanFile):
         "hsl_archive": None
     }
 
+    @property
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
@@ -51,6 +53,9 @@ class CoinHslConan(ConanFile):
         self.settings.rm_safe("compiler.libcxx")
         self.settings.rm_safe("compiler.cppstd")
 
+        if self.settings.os == "Windows":
+            self.options["msys2"].additional_packages = "mingw-w64-ucrt-x86_64-gcc-fortran"
+
     def requirements(self):
         if self.options.with_full:
             # Not needed for not with_full, e.g. ma27
@@ -58,32 +63,24 @@ class CoinHslConan(ConanFile):
             self.requires("openblas/[>=0.3.30]")
 
     def validate(self):
-        if is_msvc(self) and Version(self.version).major < 2023:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} only supports Windows build for since major version 2023")
         if is_msvc(self) and self.info.options.shared:
-            #TODO: figure out steps needed, perhaps some renaming of libcoinhsl.dll.a
-            raise ConanInvalidConfiguration(f"{self.ref} can not be built as shared on Visual Studio and msvc.")
+            raise ConanInvalidConfiguration(f"{self.ref} can not be built as shared with msvc.")
 
     def build_requirements(self):
         if Version(self.version).major > 2022:
             self.tool_requires("meson/[>=1.2.3 <2]")
         else:
-            self.tool_requires("libtool/[>=2.4.7 <3]")
+            self.tool_requires("gnu-config/cci.20210814")
+
+            if self._settings_build.os == "Windows":
+                self.win_bash = True
+                if not self.conf.get("tools.microsoft.bash:path", check_type=str):
+                    self.tool_requires("msys2/cci.latest")
+            if is_msvc(self):
+                self.tool_requires("automake/1.16.5")
 
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/[>=2.2 <3]")
-
-        #TODO: figure out what must be installed. msys2 and gfortran? Template file from CCI autotools_package is helpful
-
-        if not self.options.hsl_archive:
-            archive_uri = os.environ.get("HSL_ARCHIVE", None)
-            self.output.info("Checking environment variable HSL_ARCHIVE")
-            if archive_uri is None:
-                raise ConanInvalidConfiguration(
-                    "option:hsl_archive must point to an hsl archive file," +
-                    "url, or environment variable HSL_ARCHIVE must be set")
-            self.options.hsl_archive = archive_uri
 
     def layout(self):
         if Version(self.version).major > 2022:
@@ -96,7 +93,60 @@ class CoinHslConan(ConanFile):
             get(self, **self.conan_data["sources"][self.version]["build_scripts"], strip_root=True)
 
     def generate(self):
+
+        deps = PkgConfigDeps(self)
+        deps.generate()
+
+        if Version(self.version).major > 2022:
+            tc = MesonToolchain(self)
+            tc.generate()
+        else:
+            at = AutotoolsDeps(self)
+            at.generate()
+
+            def yes_no(v): return "yes" if v else "no"
+
+            ac = AutotoolsToolchain(self)
+            ac.configure_args.extend([
+                f"--enable-shared={yes_no(self.options.shared)}",
+                f"--enable-static={yes_no(not self.options.shared)}"
+            ])
+
+            if self.options.with_full:
+                metis = PkgConfig(self, "metis", pkg_config_path=os.path.join(self.build_folder, "conan"))
+                lapack = PkgConfig(self, "openblas", pkg_config_path=os.path.join(self.build_folder, "conan"))
+
+                ac.configure_args.extend([
+                    f'--with-lapack-lflags={" ".join(["-L" + " -L".join(lapack.libdirs), "-l" + " -l".join(lapack.libs)])}',
+                    f'--with-metis-lflags={" ".join(["-L" + " -L".join(metis.libdirs), "-l" + " -l".join(metis.libs)])}',
+                    f'--with-metis-cflags={" ".join(["-I" + " -I".join(metis.includedirs), "-D" + " -D".join(metis.defines)])}',
+                ])
+
+            env = ac.environment()
+
+            if is_msvc(self):
+                compile_wrapper = unix_path(self, self.conf.get("user.automake:compile-wrapper", check_type=str))
+                ar_wrapper = unix_path(self, self.conf.get("user.automake:lib-wrapper", check_type=str))
+                env.define("CC", f"{compile_wrapper} cl -nologo")
+                env.define("CXX", f"{compile_wrapper} cl -nologo")
+                env.define("LD", f"{compile_wrapper} link -nologo")
+                env.define("AR", f"{ar_wrapper} lib")
+                env.define("NM", "dumpbin -symbols")
+                env.define("OBJDUMP", ":")
+                env.define("RANLIB", ":")
+                env.define("STRIP", ":")
+            ac.generate(env)
+
+    def build(self):
         hsl_archive = self.options.hsl_archive
+        if not hsl_archive:
+            archive_uri = os.environ.get("HSL_ARCHIVE", None)
+            self.output.info("Checking environment variable HSL_ARCHIVE")
+            if archive_uri is None:
+                raise ConanInvalidConfiguration(
+                    "option:hsl_archive must point to an hsl archive file," +
+                    "url, or environment variable HSL_ARCHIVE must be set")
+            hsl_archive = archive_uri
 
         if hsl_archive is None:
             raise ConanInvalidConfiguration(
@@ -122,39 +172,6 @@ class CoinHslConan(ConanFile):
             unzip(self, hsl_archive, strip_root=True,
                   destination=hsl_destination)
 
-        deps = PkgConfigDeps(self)
-        deps.generate()
-
-        if Version(self.version).major > 2022:
-            tc = MesonToolchain(self)
-            tc.generate()
-        else:
-            if not cross_building(self):
-                VirtualRunEnv(self).generate(scope="build")
-
-            def yes_no(v): return "yes" if v else "no"
-
-            ac = AutotoolsToolchain(self)
-            ac.configure_args.extend([
-                f"--enable-shared={yes_no(self.options.shared)}",
-                f"--enable-static={yes_no(not self.options.shared)}"
-            ])
-
-            if self.options.with_full:
-                metis = PkgConfig(self, "metis", pkg_config_path=os.path.join(self.build_folder, "conan"))
-                lapack = PkgConfig(self, "openblas", pkg_config_path=os.path.join(self.build_folder, "conan"))
-
-                ac.configure_args.extend([
-                    f'--with-lapack-lflags={" ".join(["-L" + " -L".join(lapack.libdirs), "-l" + " -l".join(lapack.libs)])}',
-                    f'--with-metis-lflags={" ".join(["-L" + " -L".join(metis.libdirs), "-l" + " -l".join(metis.libs)])}',
-                    f'--with-metis-cflags={" ".join(["-I" + " -I".join(metis.includedirs), "-D" + " -D".join(metis.defines)])}',
-                ])
-
-            ac.generate()
-            at = AutotoolsDeps(self)
-            at.generate()
-
-    def build(self):
         if Version(self.version).major > 2022:
             meson = Meson(self)
             meson.configure()
@@ -185,8 +202,11 @@ class CoinHslConan(ConanFile):
         if self.settings.os in ["Linux", "FreeBSD"]:
             # This assumes gfortran, gcc
             self.cpp_info.system_libs.extend(["gfortran", "m"])
-            if self.options.with_full:
-                self.cpp_info.system_libs.append("gomp")  # gcc, omp for clang..
 
     def package_id(self):
         del self.info.options.hsl_archive
+
+    def system_requirements(self):
+        if self.settings.compiler == "gcc":
+            # Depends on gcc version..
+            Apt(self).install(["libgfortran5", "libquadmath0"])
